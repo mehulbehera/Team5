bcd04c27ec5ea55f3b32af2e0197107ed4c72fb6
Sonia_Sandler <sandlers@terpmail.umd.edu>
2023-01-07 20:17:45 -0500

    Updated is_pacman_in_range in Ghost to all 8 sides instead of only 4

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -20,22 +20,43 @@ public class Ghost {
   }
 
   public boolean is_pacman_in_range() {
-    Location tmp_loc = new Location(myLoc.x - 1, myLoc.y); // Location to the left
+    Location tmp_loc = new Location(myLoc.x - 1, myLoc.y - 1);
     if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
       return true;
     }
-    tmp_loc.x += 2; // Location to the right
-    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
-      return true;
-    }
-
-    tmp_loc.x = myLoc.x;
-    tmp_loc.y -= 1; //Location above
+    tmp_loc.x += 1;
     if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
       return true;
     }
 
-    tmp_loc.y += 2; //Location below
+    tmp_loc.x += 1;
+    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
+      return true;
+    }
+
+    tmp_loc.x -= 2;
+    tmp_loc.y += 1;
+    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
+      return true;
+    }
+
+    tmp_loc.x += 2;
+    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
+      return true;
+    }
+
+    tmp_loc.x -= 2;
+    tmp_loc.y += 1;
+    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
+      return true;
+    }
+
+    tmp_loc.x += 1;
+    if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
+      return true;
+    }
+
+    tmp_loc.x += 1;
     if(myMap.getLoc(tmp_loc).contains(Map.Type.PACMAN)) {
       return true;
     }
