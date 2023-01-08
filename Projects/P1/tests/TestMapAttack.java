f2417fc31b47726cea8120aa697215ed68fe038b
Sonia_Sandler <sandlers@terpmail.umd.edu>
2023-01-08 16:41:57 -0500

    Updated attack testing in TestMapAttack based on the directions in Slack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -8,18 +8,8 @@ public class TestMapAttack extends TestCase {
   public void testMapAttack() throws FileNotFoundException {
     try {
       MainFrame frame1 = new MainFrame();
-      Ghost ghost1 = frame1.addGhost(new Location(10, 9), "clyde", Color.red); //Creates a red ghost named "name" at location x,y
-      PacMan pacman1 = frame1.addPacMan(new Location(11, 9)); //Creates PacMan at location x, y
       assertTrue(frame1.getMap().attack("clyde"));
-
-      MainFrame frame2 = new MainFrame();
-      Ghost ghost2 = frame2.addGhost(new Location(10, 9), "clyde", Color.red); //Creates a red ghost named "name" at location x,y
-      PacMan pacman2 = frame2.addPacMan(new Location(7, 7)); //Creates PacMan at location x, y
-      assertTrue(!(frame2.getMap().attack("clyde")));
-
-      MainFrame frame3 = new MainFrame();
-      Ghost ghost3 = frame3.addGhost(new Location(10, 9), "clyde", Color.red); //Creates a red ghost named "name" at location x,y
-      assertTrue(!(frame3.getMap().attack("clyde")));
+      aasertTrue(frame1.isGameOver());
 
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);
