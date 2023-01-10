eb46ed48775798462051e2e608acad375335a29d
Janaki <jan29patel@gmail.com>
2023-01-09 20:58:55 -0500

    updates to getLoc

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -59,8 +59,12 @@ public class Map {
 
   public HashSet<Type> getLoc(Location loc) {
     // wallSet and emptySet will help you write this method
-    return field.get(loc);
-
+    
+   if (field.get(loc) == null){
+     return emptySet;
+   }else{
+     return field.get(loc);
+   }
   }
 
   public boolean attack(String Name) {
