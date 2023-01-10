7c33e85d4d9c4b80b80321cd4e4feffa5f20de10
Janaki <jan29patel@gmail.com>
2023-01-09 23:49:49 -0500

    fixed pacman move test

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -2,17 +2,11 @@ import java.io.*;
 import junit.framework.*;
 
 public class TestPacManMove extends TestCase {
-
-<<<<<<< HEAD
-  Mainframe frame = new MainFrame(10);
-  Cookie cookie = frame.addCookie(new Location(3, 3), "cookie");
-  PacMan pacman = frame.addPacMan(new Location(3, 4));
-  assertTrue(pacman.move(3,3) == false);
-  assertTrue(p.move(4,4) == true);
-
-=======
   public void testPacManMove() throws FileNotFoundException {
-    return null;
+    Mainframe frame = new MainFrame(10);
+    Cookie cookie = frame.addCookie(new Location(3, 3), "cookie");
+    PacMan pacman = frame.addPacMan(new Location(3, 4));
+    assertTrue(pacman.move(3,3) == false);
+    assertTrue(p.move(4,4) == true);
   }
->>>>>>> a0f239c90e1a85d30037d3de6b41b3d8a21aa8e5
 }
