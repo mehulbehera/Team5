464d83a1e60a6e6eec89fda1c71cf2baa2126c2f
Janaki <jan29patel@gmail.com>
2023-01-10 23:19:16 -0500

    added header

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -3,8 +3,10 @@ import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  MainFrame frame = new MainFrame(10);
-  Ghost ghost = frame.addGhost(new Location(3, 3), "bob", Color.red);
-  PacMan pacman = frame.addPacMan(new Location(3, 4));
-  assertTrue(ghost.move() == true);
+  public void testGhostMove() throws FileNotFoundException {
+    MainFrame frame = new MainFrame(10);
+    Ghost ghost = frame.addGhost(new Location(3, 3), "bob", Color.red);
+    PacMan pacman = frame.addPacMan(new Location(3, 4));
+    assertTrue(ghost.move() == true);
+  }
 }
