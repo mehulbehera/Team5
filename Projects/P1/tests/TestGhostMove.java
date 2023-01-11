1043efefaee4d8aa54aa70ae7166815f268a077f
Janaki <jan29patel@gmail.com>
2023-01-10 21:02:15 -0500

    Removed errors in ()

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -6,6 +6,5 @@ public class TestGhostMove extends TestCase {
   Mainframe frame = new MainFrame(10);
   Ghost ghost = frame.addGhost(new Location(3, 3), "bob", Color.red);
   PacMan pacman = frame.addPacMan(new Location(3, 4));
-  assertTrue(ghost.move(3,4) == false);
-  assertTrue(p.move(4,3) == true); 
+  assertTrue(ghost.move() == true);
 }
