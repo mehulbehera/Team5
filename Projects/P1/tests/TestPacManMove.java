6a76ce839ad47bdd66ce04567713f74ee5e36239
Janaki <jan29patel@gmail.com>
2023-01-10 23:30:05 -0500

    changed caps

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -3,7 +3,7 @@ import junit.framework.*;
 
 public class TestPacManMove extends TestCase {
   public void testPacManMove() throws FileNotFoundException {
-    Mainframe frame = new MainFrame(10);
+    MainFrame frame = new MainFrame(10);
     Cookie cookie = frame.addCookie(new Location(3, 3), "cookie");
     PacMan pacman = frame.addPacMan(new Location(3, 4));
     assertTrue(pacman.move() == true);
