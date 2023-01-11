b62872dc065e39bacc2c7c50fa64c72963f2ab62
Janaki <jan29patel@gmail.com>
2023-01-10 21:11:29 -0500

    Changed file back

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -3,9 +3,5 @@ import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  Mainframe frame = new MainFrame(10);
-  Ghost ghost = frame.addGhost(new Location(3, 3), "bob", Color.red);
-  PacMan pacman = frame.addPacMan(new Location(3, 4));
-  assertTrue(ghost.move(3,4) == false);
-  assertTrue(p.move(4,3) == true); 
+  public void testGhostMove() throws FileNotFoundException {}
 }
