9dadc8dea08b411b07137e811961836d54d4278b
tara010101 <tara010101@gmail.com>
2023-01-10 21:57:20 -0500

    Completed Map move and tests

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -1,9 +1,41 @@
 import java.io.*;
+
+import Map.Type;
 import junit.framework.*;
 
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return null;
+    //Creating A Map
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(4, 5), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y
+
+    frame.startGame();
+
+    Map map = new Map(10);
+    map.add("Ghostie", new Location(5, 5), new GhostComponent(5, 5, Color.red, 2), Type.GHOST);
+
+    assertTrue(map.move("Ghostie", new Location(5, 6), Type.GHOST));
+    assertEquals(locations.get("Ghostie"), new Location(5, 6));
+  }
+
+  public void testMapNoMove() throws FileNotFoundException {
+    //Creating A Map
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(4, 5), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y
+
+    frame.startGame();
+
+    Map map = new Map(10);
+    map.add("Cookie", new Location(5, 5), new CookieComponent(5, 5, 2), Type.COOKIE);
+
+    assertFalse(map.move("Cookie", new Location(5, 6), Type.COOKIE));
+    assertEquals(locations.get("Cookie"), new Location(5, 5));
   }
 }
