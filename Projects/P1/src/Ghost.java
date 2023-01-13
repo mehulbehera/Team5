7100a6b7a1dd2aa030a35bc710a046041600a3e2
tara010101 <tara010101@gmail.com>
2023-01-13 15:24:41 -0500

    Update Ghost.java

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -14,19 +14,17 @@ public class Ghost {
   public ArrayList<Location> get_valid_moves() {
     ArrayList<Location> lst = new ArrayList<Location>();
     if (myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.WALL)) {
-      lst.add(new Location(myLoc.x, myLoc.y + 1));
+      lst.add(new Location(myLoc.x, myLoc.y + 2));
     } 
     if (myMap.getLoc(new Location(myLoc.x, myLoc.y - 1)).contains(Map.Type.WALL)) {
-      lst.add(new Location(myLoc.x, myLoc.y - 1));
+      lst.add(new Location(myLoc.x, myLoc.y - 2));
     }
     if (myMap.getLoc(new Location(myLoc.x - 1, myLoc.y)).contains(Map.Type.WALL)) {
-      lst.add(new Location(myLoc.x - 1, myLoc.y));
+      lst.add(new Location(myLoc.x - 2, myLoc.y));
     }
     if (myMap.getLoc(new Location(myLoc.x + 1, myLoc.y)).contains(Map.Type.WALL)) {
-      lst.add(new Location(myLoc.x + 1, myLoc.y));
+      lst.add(new Location(myLoc.x + 2, myLoc.y));
     }
-	  
-    lst.add(new Location(1, 1));
 
     return lst;
   }
