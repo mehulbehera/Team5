55f51d521be3e5b35e43e2deea3dd7bbbb938850
Janaki Patel <61637458+jan29patel@users.noreply.github.com>
2023-01-13 10:20:35 -0500

    Update TestMapGetLoc.java

--- Projects/P1/tests/TestMapGetLoc.java
+++ Projects/P1/tests/TestMapGetLoc.java
@@ -5,7 +5,7 @@ public class TestMapGetLoc extends TestCase {
 
     public void testMapGetLoc() throws FileNotFoundException {
       NoFrame frame = new NoFrame();
-      assertFalse(frame.getLoc(new Location (3,3)).contains(Map.Type.GHOST));
-      assertFalse(frame.getLoc(new Location (3,3)).contains(Map.Type.COOKIE));
+      assertFalse(frame.getMap().getLoc(new Location (3,3)).contains(Map.Type.GHOST));
+      assertFalse(frame.getMap().getLoc(new Location (3,3)).contains(Map.Type.COOKIE));
     }
 }
