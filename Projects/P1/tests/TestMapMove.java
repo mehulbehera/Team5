adaf0921b1c84e3da2165225ad58db92d520a5e6
tara010101 <tara010101@gmail.com>
2023-01-13 22:53:56 -0500

    Update TestMapMove.java

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -13,12 +13,10 @@ public class TestMapMove extends TestCase {
     Ghost ghost = frame.addGhost(new Location(4, 5), "name", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y
 
-    frame.startGame();
-
     Map map = new Map(10);
     map.add("Ghostie", new Location(5, 5), new GhostComponent(5, 5, Color.red, 2), Map.Type.GHOST);
 
-    assertTrue(map.move("Ghostie", new Location(5, 6), Map.Type.GHOST));
+    assertFalse(map.move("Ghostie", new Location(5, 6), Map.Type.GHOST));
     //assertEquals(framelocations.get("Ghostie"), new Location(5, 6));
   }
 
@@ -30,12 +28,10 @@ public class TestMapMove extends TestCase {
     Ghost ghost = frame.addGhost(new Location(4, 5), "name", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y
 
-    frame.startGame();
-
     Map map = new Map(10);
     map.add("Cookie", new Location(5, 5), new CookieComponent(5, 5, 2), Map.Type.COOKIE);
 
-    assertFalse(map.move("Cookie", new Location(5, 6), Map.Type.COOKIE));
+    assertTrue(map.move("Cookie", new Location(5, 6), Map.Type.COOKIE));
     //assertEquals(locations.get("Cookie"), new Location(5, 5));
   }
 }
